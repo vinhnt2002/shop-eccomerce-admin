9ad9032cebb064f5dc63959cf27fb2774754094a
vinh nguyễn <vinhntse2002@gmail.com>
2023-07-17 19:46:46 +0700

    test fix

--- homie-admin-working/app/api/collections/route.ts
+++ homie-admin-working/app/api/collections/route.ts
@@ -5,15 +5,15 @@ import { NextResponse } from "next/server";
 // backup version
 export const POST = async (req: Request) => {
   try {
-    // const { userId } = auth();
+    const { userId } = auth();
 
     const body = await req.json();
 
     const { name, code,products } = body;
 
-    // if (!userId) {
-    //   return new NextResponse("Unauthenticated", { status: 403 });
-    // }
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 403 });
+    }
 
     if (!name) {
       return new NextResponse("Name is required", { status: 400 });
